229321b4f01b0295999c278cfb96974481379de4
mlpeck <mlpeck@arkbluecross.com>
2022-09-16 22:18:57 -0500

    setting up some base pages/layouts

---
+++ src/components/navigation.svelte
@@ -0,0 +1,8 @@
+<nav aria-label="Primary">
+  <ul>
+    <li>Home</li>
+    <li>About</li>
+    <li>Portfolio</li>
+    <li>Contact</li>
+  </ul>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { page } from '$app/stores';
+    import {writable} from 'svelte/store';
+    import Navigation from "../components/navigation.svelte";
+</script>
+
+<svelte:head>
+    <style>
+        @import url("https://fonts.googleapis.com/css?family=Raleway&display=swap");
+    </style>
+</svelte:head>
+
+<header>
+  <Navigation />
+</header>
+
+<main class="main" id="main">
+  <slot />
+</main>
+
+<footer>
+  <p>Footer content will go here some day</p>
+</footer>
+
+<style>
+  
+  header {
+    font-family: 'Raleway';
+  }
+	
+</style>
