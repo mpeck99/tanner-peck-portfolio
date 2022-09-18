dc60e5a065a2f80bca67c4d460a089f7e5f8fd34
mlpeck <mlpeck@arkbluecross.com>
2022-09-17 21:07:44 -0500

    adjusting the size of the h1 and positioning the particles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,10 @@
 </div>
 
 <style lang="scss">
-  .inner {
+  #home {
     height: 100vh;
 
-    padding: 0 8rem;
+    padding: 0 8rem; 
 
     display: flex;
     flex-direction: column;
@@ -16,5 +16,19 @@
     align-items: center;
 
     text-align: center;
+
+    h1 {
+      font-size: clamp(5.625rem, 4.773rem + 3.64vw, 7.5rem);
+      font-weight: 300;
+      margin-bottom: 0%;
+    }
+  }
+
+  #particle-div {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
   }
 </style>
